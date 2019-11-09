751c3890f166880f5d239117c47c481e5bec05b5
dfimiarz <dfimiarz@ccny.cuny.edu>
2019-11-09 06:23:30 -0500

    moved changeTimePermissions to MatchPermissions module

--- matches/permissions/MatchPermissions.js
+++ matches/permissions/MatchPermissions.js
@@ -97,6 +97,48 @@ function hasChangeCourtPermission(activityTime, curr_time = new Date()){
 
 }
 
+function checkChangeTimePermissions(orig_activity, new_activity){
+
+    //Use current time as the bases for checking permissions
+    let curr_time = new Date()
+
+    //Check if start time and end times are the same
+    if( 
+       (orig_activity.utc_start === new_activity.utc_start) && 
+       (orig_activity.utc_end === new_activity.utc_end)
+   ){
+       return "Time has not changed"
+   }
+
+   //Check if start is changing and, if so, check permission
+   if( orig_activity.utc_start !== new_activity.utc_start ){
+       
+       if( ! hasChangeStartPermission(orig_activity, curr_time) ){
+           return "Session start too far back in time"
+       }
+
+       if( ! ( new_activity.utc_start * 1000 >= (curr_time.getTime() - (10 * 60 * 1000)) )){
+           return "New start too far back in time"
+       }
+   }
+
+   //Check if end is changing and, if so, check permission
+   if( orig_activity.utc_end !== new_activity.utc_end ){
+
+       if( ! hasChangeEndPermission(orig_activity, curr_time) ){
+           return "Session end too far back in time"
+       }
+
+       if( ! ( new_activity.utc_end * 1000 >= (curr_time.getTime() - (10 * 60 * 1000)) )){
+           return "New end too far back in time"
+       }
+
+   }
+
+   return null
+
+}
+
 
 
 
@@ -106,5 +148,6 @@ module.exports = {
     hasCreatePermission,
     hasChangeEndPermission,
     hasChangeStartPermission,
-    hasChangeCourtPermission
+    hasChangeCourtPermission,
+    checkChangeTimePermissions
 }
--- matches/processor.js
+++ matches/processor.js
@@ -1,5 +1,5 @@
 const sqlconnector = require('../db/SqlConnector')
-const { hasEndPermission, hasRemovePermission, hasChangeEndPermission, hasChangeStartPermission, hasCreatePermission } = require('./permissions/MatchPermissions')
+const { hasEndPermission, hasRemovePermission, checkChangeTimePermissions } = require('./permissions/MatchPermissions')
 
 
 async function endSession(id,cmd){
@@ -196,47 +196,7 @@ async function changeSessionTime(id,cmd){
     }
 }
 
-function checkChangeTimePermissions(orig_activity, new_activity){
 
-     //Use current time as the bases for checking permissions
-     let curr_time = new Date()
-
-     //Check if start time and end times are the same
-     if( 
-        (orig_activity.utc_start === new_activity.utc_start) && 
-        (orig_activity.utc_end === new_activity.utc_end)
-    ){
-        return "Time has not changed"
-    }
-
-    //Check if start is changing and, if so, check permission
-    if( orig_activity.utc_start !== new_activity.utc_start ){
-        
-        if( ! hasChangeStartPermission(orig_activity, curr_time) ){
-            return "Session start too far back in time"
-        }
-
-        if( ! ( new_activity.utc_start * 1000 >= (curr_time.getTime() - (10 * 60 * 1000)) )){
-            return "New start too far back in time"
-        }
-    }
-
-    //Check if end is changing and, if so, check permission
-    if( orig_activity.utc_end !== new_activity.utc_end ){
-
-        if( ! hasChangeEndPermission(orig_activity, curr_time) ){
-            return "Session end too far back in time"
-        }
-
-        if( ! ( new_activity.utc_end * 1000 >= (curr_time.getTime() - (10 * 60 * 1000)) )){
-            return "New end too far back in time"
-        }
-
-    }
-
-    return null
-
-}
 
 async function changeCourt(id,cmd){
 
