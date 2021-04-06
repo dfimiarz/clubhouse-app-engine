0dde1024d523c5a37b1bf476e93a9f0b7cb0bca2
Daniel Fimiarz <dfimiarz@gmail.com>
2021-04-06 05:11:14 -0400

    Fixed HEADERS_SENT error if match does not exist

--- bookings/api.js
+++ bookings/api.js
@@ -5,7 +5,7 @@ const matchcontroller = require('./controller')
 const { PatchCommandProcessor } = require('./controller')
 const { checkMatchPermissions, validatePatchRequest } = require('./middleware')
 const MatchEventEmitter = require('./../events/MatchEmitter')
-const {authGuard} = require('../middleware/clientauth')
+const { authGuard } = require('../middleware/clientauth')
 
 
 
@@ -18,108 +18,109 @@ router.use(bodyParser.json())
 /**
  * Route to get all matches for day
  */
-router.get('/',authGuard,(req,res,next) => {
+router.get('/', authGuard, (req, res, next) => {
 
 
      const date = req.query.date ? req.query.date : null
 
      matchcontroller.getMatchesForDate(date)
-     .then((matches)=>{
-          res.send(matches)
-     })
-     .catch((err) => {
-          next(err)
-     })
-
-})
-
-router.post('/',authGuard,(req,res,next) =>{
-
-    matchcontroller.addMatch(req)
-     .then((courts)=>{
-          console.log("Count",MatchEventEmitter.listenerCount('matchadded'));
-          const val = MatchEventEmitter.emit('matchadded','test')
-          console.log( `emitted ${val}`)
-          res.status(201).send()
-     })
-     .catch((err) => {
-          next(err)
-     })
-
-    
-
-})
-
-router.get('/:id',authGuard,(req,res,next) => {
-     
-          const id = req.params.id ? req.params.id : null
-
-          matchcontroller.getMatchDetails(id)
-          .then((results)=>{ 
-
-               if( results.length !== 1 )
-                    res.json(null)
-               
-               res.locals.match = JSON.parse(results[0].match)
-               next()
-               
+          .then((matches) => {
+               res.send(matches)
           })
           .catch((err) => {
                next(err)
           })
-     },
-     checkMatchPermissions,
-     (req,res,next) => {
-
-     res.json(res.locals.match)
-     }
-)
-
-router.patch('/:id',authGuard, validatePatchRequest,
-     (req,res,next) => {
-
-     matchcontroller.processPatchCommand( req.params.id,res.locals.cmd)
-     .then((results) => {
-          res.status(204).send()
-     }).catch((err) => {
-         
-          next(err)
-     })
 
 })
 
+router.post('/', authGuard, (req, res, next) => {
 
-const generateSendSseCallback = function(res){
-     return function(message){
+     matchcontroller.addMatch(req)
+          .then((courts) => {
+               console.log("Count", MatchEventEmitter.listenerCount('matchadded'));
+               const val = MatchEventEmitter.emit('matchadded', 'test')
+               console.log(`emitted ${val}`)
+               res.status(201).send()
+          })
+          .catch((err) => {
+               next(err)
+          })
+
+
+
+})
+
+router.get('/:id', authGuard, (req, res, next) => {
+
+     const id = req.params.id ? req.params.id : null
+
+     matchcontroller.getMatchDetails(id)
+          .then((results) => {
+
+               if (! Array.isArray(results) || results.length !== 1) {
+                    return res.json(null)
+               }
+
+               res.locals.match = JSON.parse(results[0].match)
+               next()
+
+          })
+          .catch((err) => {
+               next(err)
+          })
+},
+     checkMatchPermissions,
+     (req, res, next) => {
+
+          res.json(res.locals.match)
+     }
+)
+
+router.patch('/:id', authGuard, validatePatchRequest,
+     (req, res, next) => {
+
+          matchcontroller.processPatchCommand(req.params.id, res.locals.cmd)
+               .then((results) => {
+                    res.status(204).send()
+               }).catch((err) => {
+
+                    next(err)
+               })
+
+     })
+
+
+const generateSendSseCallback = function (res) {
+     return function (message) {
           console.log(message)
           res.write(`data: ${message}\n\n`)
      }
 }
 
 
-router.get('/test',authGuard,(req,res) => {
-     res.writeHead(200,{
-         'Content-Type': 'text/event-stream',
-         'Cache-Control': 'no-cache',
-         'Connection': 'keep-alive',
-         'X-Accel-Buffering': 'no'
-    })
-    
-    res.write("Test")
- })
-
-router.get('/watch',authGuard,(req,res) => {
-
-     res.writeHead(200,{
+router.get('/test', authGuard, (req, res) => {
+     res.writeHead(200, {
           'Content-Type': 'text/event-stream',
           'Cache-Control': 'no-cache',
           'Connection': 'keep-alive',
           'X-Accel-Buffering': 'no'
      })
-     
+
+     res.write("Test")
+})
+
+router.get('/watch', authGuard, (req, res) => {
+
+     res.writeHead(200, {
+          'Content-Type': 'text/event-stream',
+          'Cache-Control': 'no-cache',
+          'Connection': 'keep-alive',
+          'X-Accel-Buffering': 'no'
+     })
+
      res.write("Test")
 
-     try{
+     try {
           // const sendFunc = generateSendSseCallback(res)
           // MatchEventEmitter.on('matchadded', sendFunc )
           req.on('close', () => {
@@ -127,7 +128,7 @@ router.get('/watch',authGuard,(req,res) => {
                // MatchEventEmitter.removeListener('matchadded',sendFunc)
           })
      }
-     catch( err ){
+     catch (err) {
           res.status(500)
      }
 })
