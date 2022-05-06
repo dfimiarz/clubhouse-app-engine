06182cab03a68a0c829c2fe1090c90e9efcd1bd5
Daniel Fimiarz <dfimiarz@gmail.com>
2022-05-06 08:34:54 -0400

    Retrieve courts for a club specified in env variable

--- courts/controller.js
+++ courts/controller.js
@@ -1,5 +1,7 @@
 const sqlconnector = require('../db/SqlConnector')
 
+const CLUB_ID = process.env.CLUB_ID;
+
 /**
  * 
  * @param { Request } request 
@@ -8,10 +10,10 @@ async function getCourts( request ){
 
     
     const connection = await sqlconnector.getConnection()
-    const query = `SELECT * FROM court`
+    const query = `SELECT * FROM court WHERE club = ?`
     try{
 
-        const courts = await sqlconnector.runQuery(connection,query)
+        const courts = await sqlconnector.runQuery(connection,query,[CLUB_ID])
         return courts
     }
     catch(error){
