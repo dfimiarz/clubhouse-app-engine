e22a4f3e36fa3f04cd420c6e533335dd82f64fb3
Daniel Fimiarz <dfimiarz@gmail.com>
2022-08-27 10:15:00 -0400

    Updated redis connector class. Fixed error reporting for expired captcha

--- auth/controller.js
+++ auth/controller.js
@@ -1,34 +1,38 @@
 const { v4: uuidv4 } = require('uuid');
-const { getAsync, setAsync } = require('./../db/RedisConnector')
+const { getClient } = require('./../db/RedisConnector')
 const svgCaptcha = require('svg-captcha')
 const RESTError = require('./../utils/RESTError');
 const sqlconnector = require('../db/SqlConnector');
 
-const EXP_TIME = 120; //Capcha expired in 120 seconds
+const EXP_TIME = 60; //Capcha expired in 120 seconds
 const ROLE_AUTHENTICATED = 1000;
 
 /**
  * 
  * @returns { Object } Captch params
  */
-async function getCaptcha(){
+async function getCaptcha() {
 
-    const captcha = svgCaptcha.create({size: 5, noise: 2});
+    const captcha = svgCaptcha.create({ size: 5, noise: 2 });
     const requestid = uuidv4();
     const text = captcha.text;
-    
-    await setAsync(requestid,text,'EX', EXP_TIME);
 
-    return {svg: encodeURIComponent(captcha.data),reqid: requestid}
+
+    await getClient().set(requestid, text, {
+        EX: EXP_TIME,
+        NX: true
+    });
+
+    return { svg: encodeURIComponent(captcha.data), reqid: requestid }
 
 }
 
-async function verifyCaptcha(requestid,text){
+async function verifyCaptcha(requestid, text) {
 
-    const res = await getAsync(requestid)
+    const res = await getClient().get(requestid);
 
-    if( ! res ){
-        throw new RESTError(422,{ epayload: [{ fielderrors: "requestid", msg: "Captcha expired" }] });
+    if (!res) {
+        throw new RESTError(422,{ fielderrors: [{ param: "captcha", msg: "Captcha expired"}]});
     }
 
     return res === text
@@ -41,27 +45,27 @@ async function verifyCaptcha(requestid,text){
  * @param {Number} club_id ClubID
  * @returns {Promise<Number>} Role for a given user
  */
-async function getUserRole(username,club_id){
+async function getUserRole(username, club_id) {
 
-    if( ! username ){
+    if (!username) {
         return null;
     }
 
     //set up connection and query
     const connection = await sqlconnector.getConnection()
     const query = `SELECT r.id,r.lbl FROM clubhouse.member m join person p on p.id = m.person_id join role r on r.id = m.role where club = ? and now() between valid_from AND valid_until and p.email = ?`;
-    
+
     //Get role from the database
     try {
 
-        const role_result = await sqlconnector.runQuery(connection, query,[club_id,username]);
+        const role_result = await sqlconnector.runQuery(connection, query, [club_id, username]);
 
-        if( !Array.isArray(role_result) || role_result.length > 1){
+        if (!Array.isArray(role_result) || role_result.length > 1) {
             throw new Error("Unexpected Result");
         }
 
-        return role_result.length === 1 ? role_result[0].id : null; 
-        
+        return role_result.length === 1 ? role_result[0].id : null;
+
     }
     catch (error) {
         throw error
--- db/RedisConnector.js
+++ db/RedisConnector.js
@@ -1,22 +1,66 @@
 const redis = require("redis");
-const {promisify} = require('util');
+const { cloudLog, localLog, cloudLogLevels: loglevels } = require('./../utils/logger/logger');
+
+/**
+ * Current number of attempts to report/log error state
+ */
+let error_log_count = 0;
+
+/**
+ * Maxium number of error reports/logs to send
+ */
+const ERROR_LOG_LIMIT = 5;
+
+/**
+ * A flag tracking reporting of connection ready state
+ */
+let connect_reported = false;
 
 const client = redis.createClient({
+  socket: {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT || 6379,
-    auth_pass: process.env.REDIS_PASSWORD
+  },
+  password: process.env.REDIS_PASSWORD
 });
 
-client.on("error", function(error) {
-  console.error(error);
+client.on("error", function (error) {
+
+  if (error_log_count < ERROR_LOG_LIMIT) {
+    error_log_count++;
+    cloudLog(loglevels.emergancy, `Redis connection error: ${error.message}. ERROR_COUNT: ${error_log_count}`)
+  }
+
+  connect_reported = false;
 });
 
-const getAsync = promisify(client.get).bind(client);
-const setAsync = promisify(client.set).bind(client);
-const getDelAsync = promisify(client.set).bind(client);
+client.on("ready", function () {
+  if (!connect_reported) {
+    cloudLog(loglevels.info, `Redis connection ready`);
+    connect_reported = true;
+  }
+  error_log_count = 0;
+
+})
+
+try {
+  connect();
+} catch (err) {
+  console.log(err);
+}
+
+async function connect() {
+  await client.connect();
+}
+
+/**
+ * 
+ * @returns Redis client object
+ */
+function getClient() {
+  return client;
+}
 
 module.exports = {
-    getAsync,
-    setAsync,
-    getDelAsync
+  getClient
 }
