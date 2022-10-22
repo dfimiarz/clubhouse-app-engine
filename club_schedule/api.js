4d98363d03e324ff038e6d9e72da05bd0c5df646
Daniel Fimiarz <dfimiarz@gmail.com>
2022-10-22 10:25:13 -0400

    Fixed formatting and remove extra code

--- club_schedule/api.js
+++ club_schedule/api.js
@@ -1,35 +1,33 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const controller = require('./controller')
-const {authGuard} = require('../middleware/clientauth')
-const { query } = require('express-validator')
+const { authGuard } = require('../middleware/clientauth')
 
 const router = express.Router();
 
-urlEncodedParse = bodyParser.urlencoded({ extended: false })
 router.use(bodyParser.json())
 
 /**
  * Route to get all court schedules
  */
-router.get('/',authGuard,(req,res,next) => {
+router.get('/', authGuard, (req, res, next) => {
 
      controller.getClubSchedules()
-     .then((schedules)=>{
-          res.json(schedules)
-     })
-     .catch((err) => {
-          next(err)
-     })
+          .then((schedules) => {
+               res.json(schedules)
+          })
+          .catch((err) => {
+               next(err)
+          })
 
 })
 
 /**
  * Route to get all court schedules
  */
- router.get('/current',authGuard,(req,res,next) => {
+router.get('/current', authGuard, (req, res, _next) => {
 
-    res.json("current_schedule");
+     res.json("current_schedule");
 
 })
 
