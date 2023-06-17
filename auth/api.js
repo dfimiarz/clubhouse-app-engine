ee389b76a7ca49ba1fdd144f4258209386f0b011
Daniel Fimiarz <dfimiarz@gmail.com>
2023-06-17 19:46:49 -0400

    Formatting fixes

--- auth/api.js
+++ auth/api.js
@@ -1,41 +1,41 @@
-const express = require('express')
-const RESTError = require('../utils/RESTError')
-const rateLimiter = require('../rate-limiter/rate-limiter')
-const authcontroller = require('./controller')
-
+const express = require("express");
+const RESTError = require("../utils/RESTError");
+const rateLimiter = require("../rate-limiter/rate-limiter");
+const authcontroller = require("./controller");
 
 const router = express.Router();
 
-
-
-router.use(express.json())
+router.use(express.json());
 
 /**
  * Route to check for geoauth
  */
-router.get('/geo', (req, res, _next) => {
+router.get("/geo", (req, res, _next) => {
+  res.json({ geoauth: res.locals.geoauth });
+});
 
-    res.json({ geoauth: res.locals.geoauth })
+router.get("/user/profile", (req, res, _next) => {
+  res.json({
+    role: res.locals.role ? res.locals.role : null,
+    geoauth: res.locals.geoauth,
+  });
+});
 
-})
-
-router.get('/user/profile', (req, res, _next) => {
-
-    res.json({ role: res.locals.role ? res.locals.role : null, geoauth: res.locals.geoauth })
-
-})
-
-router.get('/captcha', rateLimiter.captchalimiter, async (req, res, next) => {
-
-    authcontroller.getCaptcha()
-        .then((val) => {
-            res.json(val);
+router.get("/captcha", rateLimiter.captchalimiter, async (req, res, next) => {
+  authcontroller
+    .getCaptcha()
+    .then((val) => {
+      res.json(val);
+    })
+    .catch((_err) => {
+      return next(
+        new RESTError(422, {
+          fielderrors: [
+            { param: "requestid", msg: "Unable to generate captcha" },
+          ],
         })
-        .catch((_err) => {
-            return next(new RESTError(422, { fielderrors: [{ param: "requestid", msg: "Unable to generate captcha" }] }));
-        })
+      );
+    });
+});
 
-})
-
-module.exports = router
-
+module.exports = router;
--- server.js
+++ server.js
@@ -1,77 +1,79 @@
-'use strict'
+"use strict";
 
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express')
-const createError = require('http-errors')
-const cors = require('cors')
-const compression = require('compression')
-const app = express()
-const RESTError = require('./utils/RESTError')
-const { checkUserAuth, checkGeoAuth, checkUserRole } = require('./middleware/clientauth')
-const { cloudLog, cloudLogLevels: loglevels } = require('./utils/logger/logger');
+const express = require("express");
+const createError = require("http-errors");
+const cors = require("cors");
+const compression = require("compression");
+const app = express();
+const RESTError = require("./utils/RESTError");
+const {
+  checkUserAuth,
+  checkGeoAuth,
+  checkUserRole,
+} = require("./middleware/clientauth");
+const {
+  cloudLog,
+  cloudLogLevels: loglevels,
+} = require("./utils/logger/logger");
 
-app.set('trust proxy', true)
+app.set("trust proxy", true);
 
 console.log("Permitted client", process.env.CLIENT_URL);
 
 const corsOptions = {
-    origin: process.env.CLIENT_URL,
-    optionsSuccessStatus: 200,
-    credentials: true,
-    exposedHeaders: ['Etag']
-}
+  origin: process.env.CLIENT_URL,
+  optionsSuccessStatus: 200,
+  credentials: true,
+  exposedHeaders: ["Etag"],
+};
 
-app.use(compression())
-app.use(cors(corsOptions))
+app.use(compression());
+app.use(cors(corsOptions));
 
-app.use(checkGeoAuth, checkUserAuth, checkUserRole)
+app.use(checkGeoAuth, checkUserAuth, checkUserRole);
 
-app.get('/', (_req, res) => {
+app.get("/", (_req, res) => {
+  res.json({
+    name: "Knicks-Tennis API",
+    version: "1.0",
+  });
+});
+app.use("/alive", (_req, res, _next) => {
+  res.status(200).json({ status: "ok" });
+});
 
-    res.json(
-        {
-            name: 'Knicks-Tennis API',
-            version: '1.0'
-        }
-    )
-})
-app.use('/alive', (_req, res, _next) => {
-    res.status(200).json({ status: "ok" })
-})
-
-app.use('/courts', require('./courts/api'));
-app.use('/bookings', require('./bookings/api'));
-app.use('/persons', require('./persons/api'));
-app.use('/auth', require('./auth/api'));
-app.use('/guest_activations', require('./guest_activations/api'));
-app.use('/booking_types', require('./booking_types/api'));
-app.use('/club_schedule', require('./club_schedule/api.js'));
-app.use('/club', require('./club/api'));
-app.use('/reports', require('./reports/api'));
-app.use('/activities', require('./activities/api'));
+app.use("/courts", require("./courts/api"));
+app.use("/bookings", require("./bookings/api"));
+app.use("/persons", require("./persons/api"));
+app.use("/auth", require("./auth/api"));
+app.use("/guest_activations", require("./guest_activations/api"));
+app.use("/booking_types", require("./booking_types/api"));
+app.use("/club_schedule", require("./club_schedule/api.js"));
+app.use("/club", require("./club/api"));
+app.use("/reports", require("./reports/api"));
+app.use("/activities", require("./activities/api"));
+app.use("/guest_passes", require("./guest_passes/api"));
 
 app.use((_req, _res, next) => {
-    next(createError(404))
-})
+  next(createError(404));
+});
 
 app.use((err, _req, res, _next) => {
+  if (err instanceof RESTError) {
+    res.status(err.status).json(err.payload);
+  } else {
+    cloudLog(loglevels.error, err.message);
+    res.status(err.status || 500).json(err.message || "Something went wrong");
+  }
+});
 
-    if (err instanceof RESTError) {
-        res.status(err.status).json(err.payload)
-    } else {
-        cloudLog(loglevels.error, err.message);
-        res.status(err.status || 500).json(err.message || 'Something went wrong')
-    }
-
-})
-
-
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`)
-    console.log(`Press Ctrl+C to quit`)
-})
+  console.log(`Server listening on port ${PORT}...`);
+  console.log(`Press Ctrl+C to quit`);
+});
 
-module.exports = app
+module.exports = app;
