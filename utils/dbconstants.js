506bcb954bbad20257b8916a55b3785575076bdb
Daniel Fimiarz <dfimiarz@gmail.com>
2023-06-17 21:57:12 -0400

    Added data typing using jsdoc and implemented system constants

---
+++ utils/dbconstants.js
@@ -0,0 +1,30 @@
+// Desc: Constants used in the application
+
+module.exports = Object.freeze({
+  BOOKING_TYPES: Object.freeze({
+    MATCH: 1000,
+    LESSON: 5000,
+    TOURNAMENT: 6000,
+    MAINTENANCE: 7000,
+    RAIN: 7001,
+    CLOSED: 7002,
+    EVENT: 8000,
+    MENTORING: 8001,
+    JUNIOR_PROGRAM: 8002,
+    LADDER: 8003,
+  }),
+  ROLES: Object.freeze({
+    ADMIN: 4000,
+    MANAGER: 3000,
+    PROFESSIONAL: 2500,
+    MEMBER: 2000,
+    JUNIOR: 1500,
+    RETRICTED_MEMBER: 1000,
+  }),
+  ROLE_TYPES: Object.freeze({
+    GUEST_TYPE: 100,
+    RESTRICTED_MEMBER_TYPE: 200,
+    MEMBER_TYPE: 300,
+    MANAGER_TYPE: 400,
+  }),
+});
